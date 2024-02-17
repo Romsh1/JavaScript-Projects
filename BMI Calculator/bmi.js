fcd8831da66f97e1e8ec9f89f60b05126cd0f9ca
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-02-17 00:45:50 -0500

    Add files via upload
    - Created an UI for BMI Calculator - (Day 13 of learning JS)

---
+++ BMI Calculator/bmi.js
@@ -0,0 +1,21 @@
+// Romika Chaudhary
+// BMI Calculator: Day-13
+// Feb 16, 2024
+
+const btnBmi = document.querySelector("submit");
+btnBmi.addEventListener("click", 
+    result = () => {
+        console.log("clicked");
+    });
+
+const weight = document.getElementById("weight");
+const inputValue = weight.value;
+console.log(inputValue);
+
+const feet = document.getElementById("ft");
+const inputValue2 = feet.value;
+console.log(inputValue2);
+
+const inch = document.getElementById("inch");
+const inputValue3 = feet.value;
+console.log(inputValue3);
