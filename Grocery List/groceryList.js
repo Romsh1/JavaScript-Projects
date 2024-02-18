927b349b4619469c0d468c1929a4a72e02c95329
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-02-18 14:54:58 -0500

    Add files via upload
    An application to add, update and delete grocery list (Day 15 of learning JS from Scratch.

---
+++ Grocery List/groceryList.js
@@ -0,0 +1,23 @@
+// Romika Chaudhary
+// Feb 18, 2024
+// Day 15
+
+
+const btn = document.getElementById('submit');
+btn.addEventListener('click', () => {
+    const addItems = document.getElementById('items');
+    const msg = document.getElementById('result');
+    msg.innerHTML = addItems.value;
+
+    // let count = 1;
+    // for (count = 1; count <= addItems.count; count++){
+    //     msg.innerHTML = addItems.value;
+
+    // }
+
+
+    //Displaying message using backticks
+    // msg.innerHTML = `You have added ${addItems.value} to your list.`;
+    // console.log(addItems.value);
+
+});
