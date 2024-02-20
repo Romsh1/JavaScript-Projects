390e8c2b7f95021d3e21eb9b6d2305f0a81c024f
Romika <romika2002chau.21@gmail.com>
2024-02-19 21:59:36 -0500

    Day 16 - Donw with CRUD operation

--- Grocery List/groceryList.js
+++ Grocery List/groceryList.js
@@ -2,22 +2,75 @@
 // Feb 18, 2024
 // Day 15
 
+let myForm = document.querySelector("form");
+let items = document.getElementById("items");
+let result = document.getElementById("result");
+let btn = document.getElementById("submit");
 
-const btn = document.getElementById('submit');
-btn.addEventListener('click', () => {
-    const addItems = document.getElementById('items');
-    const msg = document.getElementById('result');
-    msg.innerHTML = addItems.value;
+let data = {};
 
-    // let count = 1;
-    // for (count = 1; count <= addItems.count; count++){
-    //     msg.innerHTML = addItems.value;
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
 
-    // }
-
-
-    //Displaying message using backticks
-    msg.innerHTML = `You have added ${addItems.value} to your list.`;
-    console.log(addItems.value);
-
+    formValidation();
 });
+
+let formValidation = () => {
+    if (items.value === ""){
+        alert("Items can not be blank");
+        console.log("failure");
+    } else {
+        // console.log("success");
+        // result.innerHTML = "";
+        acceptData();
+    }
+};
+
+let acceptData = () => {
+    // data["text"] = items.value;
+    // console.log(data);
+    addItems();
+};
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+    items.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+};
+
+let addItems = () => {
+    result.innerHTML += `
+    <div>
+        <p>${items.value}</p>
+        <span class="options">
+            <i onClick="editPost(this)" class="fas fa-edit"></i>
+            <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+    </div>
+    `;
+    items.value = "";
+};
+
+
+// const btn = document.getElementById('submit');
+// btn.addEventListener('click', () => {
+//     const addItems = document.getElementById('items');
+//     const msg = document.getElementById('result');
+//     msg.innerHTML += addItems.value;
+
+//     // let count = 1;
+//     // for (count = 1; count <= addItems.count; count++){
+//     //     msg.innerHTML = addItems.value;
+
+//     // }
+
+
+//     //Displaying message using backticks
+//     // msg.innerHTML = `You have added ${addItems.value} to your list.`;
+//     console.log(addItems.value);
+
+// });
