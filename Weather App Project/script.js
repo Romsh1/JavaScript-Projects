afa7e683528c356c780c817215915e8833fc982e
Romika <romika2002chau.21@gmail.com>
2024-02-26 00:23:51 -0500

    1. Worked on API Call and functionalities like displaying temperature 2. Tempertaure can be displayed both in degree celcius and degree fahrenheit

--- Weather App Project/script.js
+++ Weather App Project/script.js
@@ -9,10 +9,43 @@ let weatherForecast = document.querySelector(".forecast_weather");
 let weather_temperature = document.querySelector(".weather_temperature");
 let weatherIcon = document.querySelector(".icon_weather");
 let weatherMinMax = document.querySelector(".minMax_weather");
-// let weatherFeelsLike = document.querySelector(".weather_feels_like");
+let weatherFeelsLike = document.querySelector(".weather_feels_like");
 let weatherHumidity = document.querySelector(".weather_humidity");
 let weatherWind = document.querySelector(".weather_wind");
 let weatherPressure = document.querySelector(".weather_gauge");
+
+//Search function
+document.querySelector(".search-weather").addEventListener
+('submit', e => {
+    let search = document.querySelector(".weather_searchForm");
+    //Preventing default action
+    e.preventDefault();
+    //Changes current city 
+    curr_city = search.value;
+    //Gets the weather forecast
+    getWeather();
+    //Clearing form
+    search.value = "";
+})
+
+//Celcius to Fahrenheit and Vice-Versa
+document.querySelector(".weather_temp_celcius").addEventListener
+('click', () => {
+    if(units !== "metric"){
+        //Changing to metric
+        units = "metric"
+        //Getting the weather forecast
+        getWeather();
+    }
+})
+
+document.querySelector(".weather_temp_fahrenheit").addEventListener
+('click', () => {
+    if(units !== "imperial"){
+        units = "imperial"
+        getWeather();
+    }
+})
 
 function convertTimeStamp(timestamp, timezone) {
     //Converting seconds to hour
@@ -49,13 +82,13 @@ function getWeather() {
             city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`;
             dateTime.innerHTML = convertTimeStamp(data.dt, data.timezone);
             weatherForecast.innerHTML = `<p>${data.weather[0].main}</p>`;
-            weather_temperature.innerHTML = `${data.main.temp.toFixed()}&#176`;
+            weather_temperature.innerHTML = `${data.main.temp.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
             weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather Icon"/>`;
             weatherMinMax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176 </p>
                                         <p>Max: ${data.main.temp_max.toFixed()}&#176 </p>`;
-            // weatherFeelsLike.innerHTML = `${data.main.feels_like.toFixed()}&#176`;
+            weatherFeelsLike.innerHTML = `${data.main.feels_like.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
             weatherHumidity.innerHTML = `${data.main.humidity}%`;
-            weatherWind.innerHTML = `${data.wind.speed}m/s`;
+            weatherWind.innerHTML = `${data.wind.speed}${units === "imperial" ? "mph" : "m/s"}`;
             weatherPressure.innerHTML = `${data.main.pressure}hpa`;
         })
         .catch(error => {
