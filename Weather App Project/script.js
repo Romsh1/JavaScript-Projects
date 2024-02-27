d73c2454f21dd2fa699d3aaccb31fb44d10fcfea
Romika <romika2002chau.21@gmail.com>
2024-02-27 03:02:08 -0500

    Completed UI design and all the functionalities - Day 24 of Learning JS

--- Weather App Project/script.js
+++ Weather App Project/script.js
@@ -1,8 +1,12 @@
-//State
-let curr_city = "London";
+// Romika Chaudhary
+// Feb 22, 2024
+// File Name: script.js 
+
+//Declaring current city and units
+let curr_city = "Sarnia";
 let units = "metric";
 
-//Selectors
+//Accessing Selectors
 let city = document.querySelector(".weather_search_city");
 let dateTime = document.querySelector(".weather_dateNtime");
 let weatherForecast = document.querySelector(".forecast_weather");
@@ -14,21 +18,24 @@ let weatherHumidity = document.querySelector(".weather_humidity");
 let weatherWind = document.querySelector(".weather_wind");
 let weatherPressure = document.querySelector(".weather_gauge");
 
-//Search function
+//Search functionality
 document.querySelector(".search-weather").addEventListener
 ('submit', e => {
     let search = document.querySelector(".weather_searchForm");
     //Preventing default action
     e.preventDefault();
-    //Changes current city 
+    //Changes current city according to the value entered
     curr_city = search.value;
+    // if(search.value === ""){
+    //     alert("Please enter a city name");
+    // }
     //Gets the weather forecast
     getWeather();
-    //Clearing form
+    //Clearing input text box after each search
     search.value = "";
 })
 
-//Celcius to Fahrenheit and Vice-Versa
+//Converting Celcius to Fahrenheit and Vice-Versa
 document.querySelector(".weather_temp_celcius").addEventListener
 ('click', () => {
     if(units !== "metric"){
@@ -47,6 +54,7 @@ document.querySelector(".weather_temp_fahrenheit").addEventListener
     }
 })
 
+
 function convertTimeStamp(timestamp, timezone) {
     //Converting seconds to hour
     const convertTimezone = timezone / 3600;
@@ -60,40 +68,53 @@ function convertTimeStamp(timestamp, timezone) {
         year: "numeric",
         hour: "numeric",
         minute: "numeric",
+        //Setting the timezone for date formatting
         timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+        //Specifying to use 12 hour time format
         hour12: true,
     };
     return date.toLocaleString("en-US", options);
 }
 
-//Converting country code to name
+//Converting country code to Country name
 function convertCountryCode(country) {
     let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+    //Returns the display name of provided country codes
     return regionNames.of(country);
 }
 
+//Main function with API call
 function getWeather() {
     const API_KEY = 'b0bd8bbd5f515361fdc416b758befdc5';
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${curr_city}&appid=${API_KEY}&units=${units}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`;
-            dateTime.innerHTML = convertTimeStamp(data.dt, data.timezone);
-            weatherForecast.innerHTML = `<p>${data.weather[0].main}</p>`;
-            weather_temperature.innerHTML = `${data.main.temp.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
-            weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather Icon"/>`;
-            weatherMinMax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176 </p>
-                                        <p>Max: ${data.main.temp_max.toFixed()}&#176 </p>`;
-            weatherFeelsLike.innerHTML = `${data.main.feels_like.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
-            weatherHumidity.innerHTML = `${data.main.humidity}%`;
-            weatherWind.innerHTML = `${data.wind.speed}${units === "imperial" ? "mph" : "m/s"}`;
-            weatherPressure.innerHTML = `${data.main.pressure}hpa`;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('City not found');
+        }
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        // Process the received data
+        city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`;
+        dateTime.innerHTML = convertTimeStamp(data.dt, data.timezone);
+        weatherForecast.innerHTML = `<p>${data.weather[0].main}</p>`;
+        weather_temperature.innerHTML = `${data.main.temp.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
+        weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather Icon"/>`;
+        weatherMinMax.innerHTML = `<p>H: ${data.main.temp_min.toFixed()}&#176 </p>
+                                    <p>L: ${data.main.temp_max.toFixed()}&#176 </p>`;
+        weatherFeelsLike.innerHTML = `${data.main.feels_like.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
+        weatherHumidity.innerHTML = `${data.main.humidity}%`;
+        weatherWind.innerHTML = `${data.wind.speed}${units === "imperial" ? "mph" : "m/s"}`;
+        weatherPressure.innerHTML = `${data.main.pressure}hpa`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather data:', error);
+        if (error.message === 'City not found') {
+            alert("Invalid city name!");
+        }
+    });
 }
-
+//After the doc will be fully loaded and parsed, getWeather() function will be called
 document.addEventListener("DOMContentLoaded", getWeather);
