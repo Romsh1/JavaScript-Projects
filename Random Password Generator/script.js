3b7c218c7f470d65abece34b4e7afad0bf8108bf
Romika <romika2002chau.21@gmail.com>
2024-02-28 00:19:28 -0500

    Added a loading state - Day 24

---
+++ Random Password Generator/script.js
@@ -0,0 +1,10 @@
+// Romika Chaudhary
+// Feb 27, 2024
+// Day - 24 of Learning JS
+
+let button = document.querySelector(".gen_btn");
+let password_gen = document.querySelector(".userInput");
+
+function randomNum() {
+    
+}
