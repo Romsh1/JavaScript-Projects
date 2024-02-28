2ade7bc9fe57d05e33a1fa3e63c00b4ccc8ab4f5
Romika <romika2002chau.21@gmail.com>
2024-02-28 12:17:15 -0500

    Password generator using HTML, CSS and JS. In JS fat arrow function has been used. (Day 25 of learning JS)

--- Random Password Generator/script.js
+++ Random Password Generator/script.js
@@ -1,10 +1,18 @@
 // Romika Chaudhary
 // Feb 27, 2024
-// Day - 24 of Learning JS
+// Day - 25 of Learning JS
 
 let button = document.querySelector(".gen_btn");
 let password_gen = document.querySelector(".userInput");
 
-function randomNum() {
-    
-}
+button.addEventListener('click', () => {
+        password_gen.value = '';
+        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+        for (i = 1; i <= 6; i++){
+            let char = Math.floor(Math.random() * str.length + 1);
+            password_gen.value += str.charAt(char)
+        }
+        return password_gen.value;
+});
