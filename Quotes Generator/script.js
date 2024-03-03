5dfbf7730f6c08611d8196e1526a16cd9871f0af
Romika <romika2002chau.21@gmail.com>
2024-03-03 01:25:46 -0500

    A quote generator application in which quote can be generated by fetching data from quote api. Use of promise was understood. (Day 29 of learning JS)

--- Quotes Generator/script.js
+++ Quotes Generator/script.js
@@ -1,22 +1,45 @@
 // Romika Chaudhary
 // Feb 28, 2024
-// Day 25, 26
+// Day 25, 26, 29
 
 
+// const url = "https://api.quotable.io/random";
+// const button = document.querySelector(".btn");
+// const myQuotes = document.querySelector(".quotes");
 
-let button = document.querySelector(".btn");
-let myQuotes = document.querySelector(".quotes");
+// let quotes = [
+//     "The only way to do great work is to love what you do. - Steve Jobs",
+//     "In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+//     "Life is what happens when you're busy making other plans. - John Lennon",
+//     "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+//     "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela"
+// ];
 
-let quotes = [
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
-    "Life is what happens when you're busy making other plans. - John Lennon",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela"
-];
+// button.addEventListener('click', () => {
+//     let ranNum = Math.floor(Math.random() * quotes.length);
+//     myQuotes.value = quotes[ranNum];
+// });
+
+
+const url = "https://api.quotable.io/random";
+const button = document.querySelector(".btn");
+const myQuotes = document.querySelector(".quotes");
+const authorName = document.querySelector(".authorName");
+
 
 button.addEventListener('click', () => {
-    let ranNum = Math.floor(Math.random() * quotes.length);
-    myQuotes.value = quotes[ranNum];
+    getQuotes();
 });
 
+function getQuotes() {
+    fetch(url)
+    .then((response) => 
+        response.json())
+    .then(data => {
+        myQuotes.innerHTML = data.content;
+        authorName.innerHTML = "-- " + data.author;
+    })
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
+}
