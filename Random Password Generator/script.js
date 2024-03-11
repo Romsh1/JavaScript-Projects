f2fb55b8e48cf05fb4f52b3f7be260ccc3fca6ad
Romika <romika2002chau.21@gmail.com>
2024-03-11 00:11:34 -0400

    Created a password toggle application with a checkbox for show password. Day 33 of Learning JS

--- Random Password Generator/script.js
+++ Random Password Generator/script.js
@@ -6,13 +6,15 @@ let button = document.querySelector(".gen_btn");
 let password_gen = document.querySelector(".userInput");
 
 button.addEventListener('click', () => {
-        password_gen.value = '';
-        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+    password_gen.value = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+    'abcdefghijklmnopqrstuvwxyz0123456789@#$';
 
-        for (i = 1; i <= 6; i++){
-            let char = Math.floor(Math.random() * str.length + 1);
-            password_gen.value += str.charAt(char)
-        }
-        return password_gen.value;
+    for (i = 1; i <= 6; i++){
+        let char = Math.floor(Math.random() * str.length + 1);
+        password_gen.value += str.charAt(char)
+    }
+    return password_gen.value;
 }); 
+
+
