c46a4789f8c26ea277a5b8da2bd5a4606447d915
Romika <romika2002chau.21@gmail.com>
2024-03-12 01:40:57 -0400

    An UI for music player - Day 34 of Learning JS

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,4 +1,9 @@
 // Romika Chaudhary
 // C0921918
-// March 10, 2024
+// March 10, 12 2024
 
+var audio = document.getElementById("music");
+var time = document.querySelector(".time");
+
+document.querySelector(".play").addEventListener("click", play);
+document.querySelector(".pause").addEventListener("click", pause);
